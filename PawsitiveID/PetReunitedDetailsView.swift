c3c49df544b728509e1d881c71bb64bc9b386b2b
bitngu <bitngu3@gmail.com>
2025-07-30 20:29:08 -0700

    fix: details pet view

--- PawsitiveID/PetReunitedDetailsView.swift
+++ PawsitiveID/PetReunitedDetailsView.swift
@@ -9,55 +9,103 @@ import SwiftUI
 
 struct PetReunitedDetailsView: View {
     @Binding var pet: PetData
+    @State var photoIndex = 0
 
     var body: some View {
         VStack {
-            Form {
-                if !pet.reunited_images.isEmpty {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(pet.reunited_images, id: \.self) { photo in
-                                AsyncImage(url: URL(string: photo)) {
-                                    result in
-                                    result.image?
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(
-                                            width: 280,
-                                            height: 250,
-                                            alignment: .center
-                                        )
-                                        .clipped()
+            ZStack(alignment: .bottom) {
+                AsyncImage(
+                    url: URL(
+                        string: pet.reunited_images.count > photoIndex
+                            ? pet.reunited_images[photoIndex] : ""
+                    )
+                ) { result in
+                    result.image?
+                        .resizable()
+                        .scaledToFill()
+                        .frame(
+                            minWidth: 0,
+                            maxWidth: .infinity,
+                            alignment: .top
+                        )
+                        .containerRelativeFrame(
+                            .vertical,
+                            count: 100,
+                            span: 40,
+                            spacing: 0
+                        )
+                        .clipped()
+                }.gesture(
+                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onEnded({ value in
+
+                            if value.translation.width < 0 {
+                                photoIndex += 1
+                                if photoIndex > pet.reunited_images.count - 1 {
+                                    photoIndex = 0
                                 }
                             }
+
+                            if value.translation.width > 0 {
+                                photoIndex -= 1
+                                if photoIndex < 0 {
+                                    photoIndex =
+                                        pet.reunited_images.count == 0
+                                        ? 0 : pet.reunited_images.count - 1
+                                }
+
+                            }
+                        })
+                )
+                HStack {
+                    HStack {
+                        ForEach(0...pet.reunited_images.count - 1, id: \.self) {
+                            count in
+                            let isActive = count == photoIndex
+                            Circle()
+                                .frame(
+                                    width: isActive ? 10 : 8,
+                                    height: isActive ? 10 : 8
+                                )
+                                .padding([.horizontal], 2)
+                                .foregroundColor(
+                                    isActive
+                                        ? Color("TextSmall")
+                                        : Color("TextSmall").opacity(0.8)
+                                )
                         }
-                    }
-                }
-                Section {
-                    LabeledContent(
-                        "Name",
-                        value: pet.name.isEmpty ? "Reunited pet" : pet.name
-                    )
-                    LabeledContent(
-                        "Animal type",
-                        value: getPetType(type: pet.animal_type)
-                    )
-                    if pet.reunited_date != nil {
-                        LabeledContent(
-                            "Date",
-                            value: getFormattedDate(pet.reunited_date ?? "")
+                    }.padding([.vertical], 3)
+                        .padding([.horizontal], 6).background(
+                            Color("Background")
                         )
-                    }
-                    if pet.reunited_description != nil {
-                        Text(pet.reunited_description ?? "")
-                    }
-                } header: {
-                    Text("Reunited pet").foregroundStyle(Color("TextSmall"))
-                }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }.padding([.bottom], 10)
             }
-            .scrollContentBackground(.hidden)
-            .background(Color("Background"))
+            VStack(alignment: .leading, spacing: 0) {
+                Image("Quote")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, alignment: .center)
+                    .clipped()
+                    .padding([.leading], 10)
+                ScrollView(.vertical) {
+                    Text(pet.reunited_description ?? "")
+                        .font(.body)
+                        .foregroundStyle(Color("Text"))
+                }
+                .padding(20)
+                .padding([.leading], 40)
+                .containerRelativeFrame(
+                    .vertical,
+                    count: 100,
+                    span: 50,
+                    spacing: 0
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Spacer()
         }
+        .background(Color("Background"))
     }
 }
 
